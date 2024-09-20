092ae4f4996e05d6c8aff9bc1a550d5079a0fe31
AHMED56-16 <161157786+AHMED56-16@users.noreply.github.com>
2024-09-20 20:37:46 +0500

    COntent of 1st and 2nd class

---
+++ CLASSES/class-2/app/category/sports/page.tsx
@@ -0,0 +1,9 @@
+import Navbar from '@/app/components/navbar';
+export default function Sports(){
+    return(
+        <>
+        <Navbar/>
+        <h1>Bobzy the King</h1>
+        </>
+    )
+}
---
+++ CLASSES/class-2/app/category/world/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from '@/app/components/navbar';
+import Link from 'next/link';
+export default function World(){
+    return(
+        <>
+        <Navbar/>
+        <h1>Hello World</h1>
+        </>
+    )
+}
